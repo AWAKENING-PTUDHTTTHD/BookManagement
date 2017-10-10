816ee64910a76369abfb8ead254df84be0ca87f0
1412183 <1412183PQHoang@gmail.com>
2017-10-10 19:38:05 +0700

    re-init project

---
+++ bookManagement/src/main/java/com/awakening/bookManagement/App.java
@@ -0,0 +1,13 @@
+package com.awakening.bookManagement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ bookManagement/src/main/java/com/awakening/bookManagement/model/Book.java
@@ -0,0 +1,121 @@
+package com.awakening.bookManagement.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "book")
+public class Book {
+	
+	private Long id;
+	/**
+	 * @return the id
+	 */
+	
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+	/**
+	 * @return the price
+	 */
+	public String getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	/**
+	 * @return the isbn
+	 */
+	public String getIsbn() {
+		return isbn;
+	}
+	/**
+	 * @param isbn the isbn to set
+	 */
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	/**
+	 * @return the publisher
+	 */
+	public String getPublisher() {
+		return publisher;
+	}
+	/**
+	 * @param publisher the publisher to set
+	 */
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	/**
+	 * @return the publishedOne
+	 */
+	public Date getPublishedOn() {
+		return publishedOn;
+	}
+	/**
+	 * @param publishedOne the publishedOn to set
+	 */
+	public void setPublishedOne(Date publishedOn) {
+		this.publishedOn = publishedOn;
+	}
+	private String name;
+	private String code;
+	private String price;
+	private String author;
+	private String isbn;
+	private String publisher;
+	private Date publishedOn;
+}
